5d1c1ab17eb9216e90264a40aea0437d0531d576
HagaSpa <justicesparrow13@gmail.com>
2020-11-17 12:51:54 +0900

    wakkanne-

--- jackanalyzer/cmplengn/compilationengine.go
+++ jackanalyzer/cmplengn/compilationengine.go
@@ -10,6 +10,20 @@ type CompilationEngine struct {
 	t token.Token
 }
 
+// expression
+
+// TODO: いやわっかんねー
+// type expression struct {
+// 	term term `xml:"term"`
+// 	op op
+// }
+// type expressionList struct {
+// 	expressions []expression `xml:"expression"`
+// }
+type op string              // '+' | '-' | '*' | '&' | '|' | '<' | '>' | '='
+type unaryOp string         // '+' | '-'
+type keywordConstant string // 'true' | 'false' | 'null' | 'this'
+
 func New(w io.Writer, t token.Token) *CompilationEngine {
 	ce := &CompilationEngine{
 		w: w,

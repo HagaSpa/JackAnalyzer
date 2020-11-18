1792ed409dbfdfadcffaacac3b6c61cc1675ec3d
HagaSpa <justicesparrow13@gmail.com>
2020-11-18 16:38:04 +0900

    defined parameter struct

--- jackanalyzer/cmplengn/compilationengine.go
+++ jackanalyzer/cmplengn/compilationengine.go
@@ -20,6 +20,13 @@ type CompilationEngine struct {
 // type expressionList struct {
 // 	expressions []expression `xml:"expression"`
 // }
+
+// Parameter is element for parameterList
+type parameter struct {
+	Keyword    string
+	Identifier string
+	Symbol     string
+}
 type op string              // '+' | '-' | '*' | '&' | '|' | '<' | '>' | '='
 type unaryOp string         // '+' | '-'
 type keywordConstant string // 'true' | 'false' | 'null' | 'this'

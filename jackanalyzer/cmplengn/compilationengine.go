46cc13a33ab2309dd1befbc7d2d657b56ced250a
HagaSpa <justicesparrow13@gmail.com>
2020-12-02 09:58:36 +0900

    exclude unnecessary structs

--- jackanalyzer/cmplengn/compilationengine.go
+++ jackanalyzer/cmplengn/compilationengine.go
@@ -10,16 +10,6 @@ type CompilationEngine struct {
 	t token.Token
 }
 
-// Parameter is element for parameterList
-type parameter struct {
-	Keyword    string
-	Identifier string
-	Symbol     string
-}
-type op string              // '+' | '-' | '*' | '&' | '|' | '<' | '>' | '='
-type unaryOp string         // '+' | '-'
-type keywordConstant string // 'true' | 'false' | 'null' | 'this'
-
 func New(w io.Writer, t token.Token) *CompilationEngine {
 	ce := &CompilationEngine{
 		w: w,

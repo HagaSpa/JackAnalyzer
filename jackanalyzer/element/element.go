739a71b6062548d3c4fdcbbdeb079aec4edd35d9
HagaSpa <justicesparrow13@gmail.com>
2020-12-07 17:03:47 +0900

    exclude VarName condition for genCotent.

--- jackanalyzer/element/element.go
+++ jackanalyzer/element/element.go
@@ -259,8 +259,6 @@ func genContent(s interface{}) string {
 		str = string(s.(identifier))
 	case symbol:
 		str = string(s.(symbol))
-	case VarName:
-		str = string(s.(VarName))
 	}
 	return " " + str + " "
 }

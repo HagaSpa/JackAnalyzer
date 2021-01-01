9ba1e9e6460a0b9e0a6f77a2717b98cd7ddcdc21
HagaSpa <justicesparrow13@gmail.com>
2021-01-01 11:08:56 +0900

    implement genElement in CompilationEngine

--- jackanalyzer/cmplengn/compilationengine.go
+++ jackanalyzer/cmplengn/compilationengine.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"io"
 	"jackanalyzer/token"
+	"strconv"
 )
 
 type CompilationEngine struct {
@@ -20,4 +21,28 @@ func New(w io.Writer, t token.Token) *CompilationEngine {
 		e: e,
 	}
 	return ce
+}
+
+// generate Element for *xml.EncodeElement.
+func genElement(t token.Token) (string, xml.StartElement) {
+	var c string // contents
+	var l string // labels
+	switch t.TokenType {
+	case token.KEYWORD:
+		c = string(t.Keyword)
+		l = "keyword"
+	case token.IDENTIFIER:
+		c = t.Identifier
+		l = "identifier"
+	case token.SYMBOL:
+		c = t.Symbol
+		l = "symbol"
+	case token.INT_CONST:
+		c = strconv.Itoa(int(t.IntVal))
+		l = "integerConstant"
+	case token.STRING_CONST:
+		c = t.StringVal
+		l = "stringConstant"
+	}
+	return " " + c + " ", xml.StartElement{Name: xml.Name{Local: l}}
 }
--- jackanalyzer/cmplengn/compilationengine_test.go
+++ jackanalyzer/cmplengn/compilationengine_test.go
@@ -2,6 +2,7 @@ package cmplengn
 
 import (
 	"bytes"
+	"encoding/xml"
 	"jackanalyzer/token"
 	"reflect"
 	"testing"
@@ -45,6 +46,36 @@ func TestNew(t *testing.T) {
 			}
 			if gotW := w.String(); gotW != tt.wantW {
 				t.Errorf("New() = %v, want %v", gotW, tt.wantW)
+			}
+		})
+	}
+}
+
+func Test_genElement(t *testing.T) {
+	tests := []struct {
+		name  string
+		t     token.Token
+		want  string
+		want1 xml.StartElement
+	}{
+		{
+			"test KEYWORD",
+			token.Token{
+				TokenType: token.KEYWORD,
+				Keyword:   token.CLASS,
+			},
+			" class ",
+			xml.StartElement{Name: xml.Name{Local: "keyword"}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := genElement(tt.t)
+			if got != tt.want {
+				t.Errorf("genElement() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("genElement() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
